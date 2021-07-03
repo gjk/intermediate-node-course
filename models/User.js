1ff73b2c813ea3f6e21d73afb06fc9974e2402fa
gu <gu.jingkun@upex.co>
2021-07-03 17:14:26 +0800

    2

--- models/User.js
+++ models/User.js
@@ -10,3 +10,26 @@ module.exports= mongoose.model('User',UserSchema)
 
 const User=require('./models/User');
 mongoose.connect('mongodb://localhost/userData')
+
+
+// READ
+.get((req,res)=>{
+  User.findById(req.params.id,(err,data)=>{
+    if (err){
+      res.json({
+        success: false,
+        message: err
+      })
+    } else if (!data){
+      res.json({
+        success: false,
+        message: "Not Found"
+      })
+    } else {
+      res.json({
+        success: true,
+        data: data
+      })
+    }
+  })
+})
