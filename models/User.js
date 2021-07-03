bd7e0ddbea4e1694806dfd7b1aead312465dbf0a
gu <gu.jingkun@upex.co>
2021-07-03 17:17:09 +0800

    4

--- models/User.js
+++ models/User.js
@@ -66,3 +66,28 @@ mongoose.connect('mongodb://localhost/userData')
     }
   )
 })
+
+// DELETE
+.delete((req,res)=>{
+  User.findByIdAndDelete(
+    req.params.id,
+    (err,data)=>{
+      if (err){
+        res.json({
+          success: false,
+          message: err
+        })
+      } else if (!data){
+        res.json({
+          success: false,
+          message: "Not Found"
+        })
+      } else {
+        res.json({
+          success: true,
+          data: data
+        })
+      }
+    }
+  )
+})
